745ace434de18b93704637f38190da16accbb4c3
rashedsabbir <rashedulhasan1996@gmail.com>
2022-05-05 17:34:47 +0600

    firebase added

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhJg7FPpuKCvofmcKEn8b983nPicnJZIc",
+  authDomain: "sareealoy.firebaseapp.com",
+  projectId: "sareealoy",
+  storageBucket: "sareealoy.appspot.com",
+  messagingSenderId: "1074875006572",
+  appId: "1:1074875006572:web:4111995d03567bb794a249"
+  };
+  export default firebaseConfig;
